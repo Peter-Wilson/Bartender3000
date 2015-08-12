7375320a347c5f594df72d1617127249d9ac539b
rippedpants <therock_rox@hotmail.com>
2015-08-12 18:28:55 -0400

    Cleaned up code

--- app/src/main/java/inventtech/bartender3000/Bartender.java
+++ app/src/main/java/inventtech/bartender3000/Bartender.java
@@ -10,10 +10,14 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.PorterDuff;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
@@ -21,7 +25,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -45,10 +51,9 @@ public class Bartender extends Activity {
     private OutputStream out;
     byte[] readBuffer;
     int readBufferPosition;
-    /* request BT enable */
     private static final int  REQUEST_ENABLE      = 0x1;
-    /* request BT discover */
-    private static final int  REQUEST_DISCOVERABLE  = 0x2;
+    private static final int REQUEST_BLU = 1;
+    private static final int DISCOVER_DURATION = 300;
     private ImageButton drink1, drink2, drink3, drink4;
 
     @Override
@@ -61,6 +66,11 @@ public class Bartender extends Activity {
         //Get the bluetooth adapter
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
+        if(bluetoothAdapter == null)
+        {
+            Toast.makeText(this, "Bluetooth is not supported on this device", Toast.LENGTH_LONG);
+        }
+
         //enable bluetooth if it isn't already
         if(!bluetoothAdapter.isEnabled())
         {
@@ -68,10 +78,15 @@ public class Bartender extends Activity {
             startActivityForResult(enabler, REQUEST_ENABLE);
         }
 
-        arduino = GetArduinoDevice();
-        arduinoSocket = getDeviceSocket(arduino);
-
+        //connect to bluetooth arduino and set up input/output streams
         try {
+
+            arduino = GetArduinoDevice();
+            if(arduino == null) {
+               // findDevice();
+                Toast.makeText(this,"You have not paired with the arduino yet",Toast.LENGTH_LONG);
+            }
+            arduinoSocket = getDeviceSocket(arduino);
             arduinoSocket.connect();
             in = arduinoSocket.getInputStream();
             out = arduinoSocket.getOutputStream();
@@ -79,123 +94,14 @@ public class Bartender extends Activity {
         catch(Exception e)
         {
             //cannot connect to the arduino
-            showAlert("Connection Unsuccessful", "Unable to connect to Arduino. Please restart and try again");
+            showAlert("Cannot Connect","Unable to connect to Arduino. Please restart and try again");
             System.out.println("Connection was unsuccessful");
-            return;
-        }
-        if (savedInstanceState == null || !savedInstanceState.getBoolean("CupThere")) {
-            setContentView(R.layout.activity_cup_scan);
-
-            // Execute some code after 2 seconds have passed
-            SetUpInputThread();
-
-
-        } else {
-            setContentView(R.layout.activity_drink_selector);
-            SetOnClickListeners();
-            SetUpInputThread();
+            //return;
         }
 
-
-
-
-
-
-       /* try
-        {
-            in = arduinoSocket.getInputStream();
-            out = arduinoSocket.getOutputStream();
-        }
-        catch(Exception e)
-        {
-            System.out.println("Cannot establish connection");
-            return;
-        }*/
-
-
-        //SetUpInputThread();
-
-    }
-
-    private void pairDevice(BluetoothDevice device) {
-        try {
-             Log.d("TAG", "Start Pairing...");
-
-            //waitingForBonding = true;
-
-            Method m = device.getClass()
-                    .getMethod("createBond", (Class[]) null);
-            m.invoke(device, (Object[]) null);
-
-            Log.d("TAG", "Pairing finished.");
-        } catch (Exception e) {
-            Log.e("TAG", e.getMessage());
-        }
-    }
-
-    private void unpairDevice(BluetoothDevice device) {
-        try {
-            Method m = device.getClass()
-                    .getMethod("removeBond", (Class[]) null);
-            m.invoke(device, (Object[]) null);
-        } catch (Exception e) {
-            Log.e("TAG", e.getMessage());
-        }
-    }
-
-    private void SetOnClickListeners() {
-        drink1 = (ImageButton) findViewById(R.id.drink1);
-        drink2 = (ImageButton) findViewById(R.id.drink2);
-        drink3 = (ImageButton) findViewById(R.id.drink3);
-        drink4 = (ImageButton) findViewById(R.id.drink4);
-
-        drink1.setOnClickListener(new View.OnClickListener() {
-                                                @Override
-                                                public void onClick(View v) {
-                                                    ButtonSelected(drink1,'a');
-                                                }
-                                            }
-        );
-
-        drink2.setOnClickListener(new View.OnClickListener() {
-                                      @Override
-                                      public void onClick(View v) {
-                                          ButtonSelected(drink2,'b');
-                                      }
-                                  }
-        );
-
-        drink3.setOnClickListener(new View.OnClickListener() {
-                                      @Override
-                                      public void onClick(View v) {
-                                          ButtonSelected(drink3,'c');
-                                      }
-                                  }
-        );
-
-        drink4.setOnClickListener(new View.OnClickListener() {
-                                      @Override
-                                      public void onClick(View v) {
-                                          ButtonSelected(drink4,'d');
-                                      }
-                                  }
-        );
-    }
-
-    protected void ButtonSelected(ImageButton button,char choice)
-    {
-        try {
-            ((ImageView) findViewById(R.id.drink1)).setColorFilter(0x80808000, PorterDuff.Mode.MULTIPLY);
-            ((ImageView) findViewById(R.id.drink2)).setColorFilter(0x80808000, PorterDuff.Mode.MULTIPLY);
-            ((ImageView) findViewById(R.id.drink3)).setColorFilter(0x80808000, PorterDuff.Mode.MULTIPLY);
-            ((ImageView) findViewById(R.id.drink4)).setColorFilter(0x80808000, PorterDuff.Mode.MULTIPLY);
-            button.clearColorFilter();
-
-            out.write(choice);
-            out.flush();
-        } catch (Exception e) {
-            showAlert("Cannot Send", "Sorry, the message cannot be sent.");
-        }
+        //load the cup scan view
+        setContentView(R.layout.activity_cup_scan);
+        SetUpInputThread();
     }
 
     protected void showAlert(String title, String message) {
@@ -211,9 +117,47 @@ public class Bartender extends Activity {
                 .show();
     }
 
+    //set up the clickListeners for the drink buttons
+    private void SetOnClickListeners() {
 
+        drink1 = (ImageButton) findViewById(R.id.drink1);
+        drink2 = (ImageButton) findViewById(R.id.drink2);
+        drink3 = (ImageButton) findViewById(R.id.drink3);
+        drink4 = (ImageButton) findViewById(R.id.drink4);
+
+        drink1.setOnClickListener(new View.OnClickListener()
+        { @Override public void onClick(View v) {ButtonSelected(drink1,'a');}});
+
+        drink2.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {ButtonSelected(drink2,'b');}});
+
+        drink3.setOnClickListener(new View.OnClickListener()
+        {@Override public void onClick(View v) {ButtonSelected(drink3,'c');}});
+
+        drink4.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {ButtonSelected(drink4,'d');}});
+    }
+
+    protected void ButtonSelected(ImageButton button,char choice)
+    {
+        try {
+            //darken all the buttons other than the one selected
+            ((ImageView) findViewById(R.id.drink1)).setColorFilter(0x80808000, PorterDuff.Mode.MULTIPLY);
+            ((ImageView) findViewById(R.id.drink2)).setColorFilter(0x80808000, PorterDuff.Mode.MULTIPLY);
+            ((ImageView) findViewById(R.id.drink3)).setColorFilter(0x80808000, PorterDuff.Mode.MULTIPLY);
+            ((ImageView) findViewById(R.id.drink4)).setColorFilter(0x80808000, PorterDuff.Mode.MULTIPLY);
+            button.clearColorFilter();
+
+            //send the selection to the arduino
+            out.write(choice);
+            out.flush();
+        } catch (Exception e) {
+            Toast.makeText(this,"Selection cannot be sent to the Bartender.", Toast.LENGTH_LONG);
+        }
+    }
+
+    //create the thread to read from the arduino
     private void SetUpInputThread() {
-            final Handler handler = new Handler();
             final byte delimiter = 10; //This is the ASCII code for a newline character
             stopWorker = false;
             readBufferPosition = 0;
@@ -244,11 +188,13 @@ public class Bartender extends Activity {
 
                                         runOnUiThread(new Runnable() {
                                             public void run() {
-                                                //showAlert("Data",data);
+                                                //the cup is ready to be filled
                                                 if (data.equals("y\r")) {
                                                     newCup();
+                                                //the cup is not ready to be filled
                                                 } else if (data.equals("n\r")) {
                                                     cupGone();
+                                                //the drink has been finished
                                                 } else if (data.equals("d\r")) {
                                                     drinkFull();
                                                 }
@@ -262,7 +208,6 @@ public class Bartender extends Activity {
                                 }
                             }
                         }
-
                         arduinoSocket.close();
                     }
                     catch(Exception e) {
@@ -306,20 +251,21 @@ public class Bartender extends Activity {
         }
         catch(Exception e)
         {
+            Toast.makeText(this,"Cannot connect to the device", Toast.LENGTH_LONG);
             return null;
         }
 
     }
+
 
     @Override
     protected void onSaveInstanceState (Bundle outState) {
         try {
             arduinoSocket.close();
         }
-        catch(Exception e)
-        {
+        catch(Exception e){     }
 
-        }
+        //save the cup state
         outState.putBoolean("CupThere", cupScanned);
         super.onSaveInstanceState(outState);
     }
@@ -345,27 +291,30 @@ public class Bartender extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    //a new cup is found
     public void newCup()
     {
         if(!cupScanned) {
             cupScanned = true;
-            showAlert("Cup Detected", "A Cup has been detected, select your drink");
+            Toast.makeText(this,"A Cup has been detected, select your drink", Toast.LENGTH_LONG);
             setContentView(R.layout.activity_drink_selector);
             SetOnClickListeners();
         }
 
     }
 
+    //the cup is removed
     public void cupGone()
     {
         if(cupScanned) {
             cupScanned = false;
-            showAlert("Cup Removed","The cup has been removed");
+            Toast.makeText(this, "The cup has been removed", Toast.LENGTH_LONG);
             setContentView(R.layout.activity_cup_scan);
         }
     }
 
+    //the drink is filled
     public void drinkFull() {
-        showAlert("Drink Full","The drink has been filled, you may remove the cup");
+        Toast.makeText(this, "The drink has been filled, you may remove the cup", Toast.LENGTH_LONG);
     }
 }
